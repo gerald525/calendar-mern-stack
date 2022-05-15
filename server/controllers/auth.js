eaeb3909b6b0650f48ca688f341ee41968126bcc
gerald525 <gerald.lloyd.us@gmail.com>
2022-05-14 22:55:47 -0500

    Added login with jwt token generation

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,5 +1,6 @@
 const bcryptjs = require("bcryptjs");
 const User = require("../models/user");
+const generateJWT = require("../helpers/jwt");
 
 const createUser = async (req, res) => {
   const { email, password } = req.body;
@@ -11,7 +12,7 @@ const createUser = async (req, res) => {
         .status(400)
         .json({ ok: false, msg: "User email already exists" });
     }
-    
+
     user = new User(req.body);
 
     // Encrypt password
@@ -20,11 +21,16 @@ const createUser = async (req, res) => {
 
     await user.save();
 
+    // Generate JWT
+    const token = await generateJWT(user.id, user.name);
+
     return res.status(201).json({
       ok: true,
       msg: "User registered successfully",
       user,
+      token
     });
+
   } catch (error) {
     console.log(error);
     return res.status(500).json({
@@ -34,14 +40,45 @@ const createUser = async (req, res) => {
   }
 };
 
-const loginUser = (req, res) => {
+const loginUser = async (req, res) => {
   const { email, password } = req.body;
 
-  res.json({
-    ok: true,
-    email,
-    password,
-  });
+  try {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({
+        ok: false, msg: "User email does not exist"
+      }
+      );
+    }
+
+    // Verify if passwords match
+    const validPassword = bcryptjs.compareSync(password, user.password);
+    if (!validPassword) {
+      return res.status(401).json({
+        ok: false,
+        msg: "Invalid password.",
+      });
+    }
+
+    // Generate JWT
+    const token = await generateJWT(user.id, user.name);
+
+    return res.status(200).json({
+      ok: true,
+      msg: "User logged successfully",
+      user,
+      token
+    });
+
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      ok: false,
+      msg: "Please, contact the administrator",
+    });
+  }
 };
 
 const renewToken = (req, res) => {
---
+++ server/helpers/jwt.js
@@ -0,0 +1,24 @@
+const jwt = require("jsonwebtoken");
+
+const generateJWT = (id, name) => {
+  return new Promise((resolve, reject) => {
+    const payload = { id, name };
+    jwt.sign(
+      payload,
+      process.env.JWT_SECRET_KEY,
+      {
+        expiresIn: "2h",
+      },
+      (err, token) => {
+        if (err) {
+          console.log("JWT Generation Error", err);
+          reject("Can't generate web token.");
+        } else {
+          resolve(token);
+        }
+      }
+    );
+  });
+};
+
+module.exports = generateJWT;
